2341827ca201e00fd5be998023ef4d1a31c0d02e
ann-droid5 <annmaria51005@gmail.com>
2026-03-11 03:14:37 +0530

    connect frontend to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-portal-backend-jh4g.onrender.com/api',
 });
 
 export default api;
