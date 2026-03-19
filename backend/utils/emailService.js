bf0f56944724b87f88c5861a65d326c40e47ebf1
ann-droid5 <annmaria51005@gmail.com>
2026-03-19 18:16:02 +0530

    resend email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,4 +1,7 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
