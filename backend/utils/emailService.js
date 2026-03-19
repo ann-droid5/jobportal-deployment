5b8003f9bea07a2d1507648ce86f7941f8560478
ann-droid5 <annmaria51005@gmail.com>
2026-03-19 13:14:42 +0530

    email configsmtp

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,12 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP Error:", error);
+  } else {
+    console.log("SMTP Ready");
+  }
+});
+
 console.log("EMAIL USER:", process.env.EMAIL_USER);
 // Welcome email after signup
 const sendWelcomeEmail = async (to, name) => {
